4887d7d5aefa03b19fc2ab66ae8eda5c65d072c5
Varun Sharma <varunsh@stepsecurity.io>
2023-05-15 15:51:51 -0700

    Update vulnerable.swift

--- vulnerable.swift
+++ vulnerable.swift
@@ -1,75 +1,35 @@
-
 import UIKit
+import SQLite3
 
-class CouchBaseExerciseVC: UIViewController {
+class SQLInjectionExerciseVC: UIViewController {
+    @IBOutlet weak var searchField: UITextField!
     
-    @IBOutlet weak var nameTextfield: UITextField!
-    @IBOutlet weak var ageTextField: UITextField!
-    @IBOutlet weak var genderTextField: UITextField!
-    @IBOutlet weak var diseaseTextField: UITextField!
-    
-    let CBKeyPatientName = "CouchKeyPatientName";
-    let CBKeyPatientAge = "CouchKeyPatientAge";
-    let CBKeyPatientGender = "CouchKeyPatientGender";
-    let CBKeyPatientDisease = "CouchKeyPatientDisease";
-    
-    let CBValuePatientName = "Jane Roe";
-    let CBValuePatientAge = "52";
-    let CBValuePatientGender = "Female";
-    let CBValuePatientDisease = "Cancer";
-    
-    var database:CBLDatabase?
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        do {
-            try database = CBLManager.sharedInstance().databaseNamed("couchbasedb")
-        } catch {
+    @IBAction func search() {
+        let dbPath = URL(fileURLWithPath: Bundle.main.resourcePath ?? "").appendingPathComponent("articles.sqlite").absoluteString
+        var db: OpaquePointer?
+        if sqlite3_open(dbPath, &db) != SQLITE_OK {
+            UIAlertController.showAlertWith(title: "Snap!", message: "Error opening articles database.")
+            return
         }
-
-        saveData()
-    }
-
-    func saveData() {
-        if UserDefaults.standard.object(forKey: "documentID") == nil {
-            let properties = [CBKeyPatientName:CBValuePatientName,
-                              CBKeyPatientAge : CBValuePatientAge,
-                              CBKeyPatientGender : CBValuePatientGender,
-                              CBKeyPatientDisease : CBValuePatientDisease]
-            
-            let newDocument = database?.createDocument()
-            
-            do {
-                try newDocument?.putProperties(properties)
-            } catch {
-            }
-            
-            UserDefaults.standard.set(newDocument?.documentID, forKey: "documentID")
+        
+        var searchStr = "%"
+        if !(searchField.text?.isEmpty ?? true) {
+             searchStr = "%" + "\(searchField.text!)" + "%"
         }
-    }
-    
-    @IBAction func verifyItemPressed() {
         
-        let info:Dictionary<String, String> = [CBKeyPatientName: self.nameTextfield.text!,
-                                               CBKeyPatientAge: self.ageTextField.text!,
-                                               CBKeyPatientGender: self.genderTextField.text!,
-                                               CBKeyPatientDisease: self.diseaseTextField.text!]
-
-        let isVerified = verifyPatientInfo(info)
-        UIAlertController.showAlertWith(title: "iGoat", message: isVerified ? "Success!!" :  "Failed")
-    }
-    
-    func verifyPatientInfo(_ info:Dictionary<String, String>) -> Bool {
-    
-        let documentId = UserDefaults.standard.object(forKey: "documentID") as! String
-        let document:CBLDocument = (database?.document(withID: documentId))!
+        let query = "SELECT title FROM article WHERE title LIKE '\(searchStr)' AND premium=0"
+        var stmt: OpaquePointer?
+        sqlite3_prepare_v2(db, query, -1, &stmt, nil)
+        var articleTitles = [String]()
+        while sqlite3_step(stmt) == SQLITE_ROW {
+            let title = String(cString: sqlite3_column_text(stmt, 0))
+            articleTitles.append(title)
+        }
+        sqlite3_finalize(stmt)
+        sqlite3_close(db)
         
-        let isEqual = (document.property(forKey: CBKeyPatientName) as? String == info[CBKeyPatientName]) &&
-            (document.property(forKey: CBKeyPatientAge) as? String == info[CBKeyPatientAge]) &&
-            (document.property(forKey: CBKeyPatientGender) as? String == info[CBKeyPatientGender]) &&
-            (document.property(forKey: CBKeyPatientDisease) as? String == info[CBKeyPatientDisease])
-
-        return isEqual
+        let sqlInjectionArticlesVC = SQLInjectionArticlesVC(nibName: "SQLInjectionArticlesVC", bundle: nil)
+        sqlInjectionArticlesVC.articles = articleTitles
+        navigationController?.pushViewController(sqlInjectionArticlesVC, animated: true)
     }
-    
 }
