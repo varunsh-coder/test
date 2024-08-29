a11b0e8deada6ecad80eb53bb02be79ead1c82f0
Varun Sharma <varunsh@stepsecurity.io>
2024-08-29 06:09:43 -0700

    Update entrypoint.sh

--- .github/actions/gcp-docker-action/entrypoint.sh
+++ .github/actions/gcp-docker-action/entrypoint.sh
@@ -3,6 +3,9 @@
 # Print the path of the Google Cloud credentials file
 echo "Google Cloud credentials file path: ${GOOGLE_APPLICATION_CREDENTIALS}"
 
+echo "Printing all environment variables:"
+env
+
 # Authenticate using the provided credentials
 gcloud auth activate-service-account --key-file="${GOOGLE_APPLICATION_CREDENTIALS}"
 
